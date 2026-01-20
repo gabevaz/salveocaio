d96313ef2dc12556574b46a3b26891905a3fb6e9
William Cesar <willcesar010421@gmail.com>
2026-01-19 23:53:08 -0300

    feat: add video in hystory section

--- src/sections/HistorySection.vue
+++ src/sections/HistorySection.vue
@@ -6,7 +6,7 @@
     <div id="history" class="container mx-auto px-5">
         <section class="py-16">
             <div class="mx-auto">
-                <div class="md:flex">
+                <div class="md:flex md:items-center">
                     <img src="/images/caiopais.jpeg" alt="pais" class="md:w-1/3 rounded-xl" />
                     <div class="space-y-4 text-justify md:ml-8 mt-8 md:mt-4">
                         <h2 class="text-3xl md:text-4xl font-semibold font-theme-heading mb-10">A história do Caio
@@ -37,7 +37,14 @@
                 </div>
 
             </div>
-
+            
+            <div class="block max-md:mt-12 mt-20 max-w-[1200px] mx-auto">
+                <h3 class="text-2xl mb-8 text-center">Reportagem R7 - Balanço Geral, publicada em: 15/01/2026 - <a class="text-blue-700 underline" href="https://record.r7.com/balanco-geral/video/menino-de-4-anos-e-diagnosticado-com-doenca-ultrarrara-e-sem-cura-15012026/" target="_blank">Link da matéria</a></h3>
+    
+                <video class="rounded-xl" width="100%" controls>
+                    <source src="/videos/reportagem-caio.mp4" type="video/mp4">
+                </video>
+            </div>
         </section>
     </div>
 </template>
